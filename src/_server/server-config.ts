ff5e27570ea5fd730db7d5973955c58d13a35851
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2019-08-22 21:49:19 +0900

    Add deployment to Lambda and S3, staging and prod

--- src/_server/server-config.ts
+++ src/_server/server-config.ts
@@ -1,11 +1,11 @@
 const stage = process.env.STAGE || ''
 
-export const staticBaseUrl = ({
-  dev: 'http://localhost:8090',
-  'local-prod': 'http://localhost:8091',
-  staging: 'http://s3-staging',
-  prod: 'http://s3-prod',
-} as any)[stage]
+export const staticBaseUrl =
+  stage === 'dev'
+    ? 'http://localhost:8090'
+    : stage === 'local-prod'
+    ? 'http://localhost:8091'
+    : `https://${process.env.S3_BUCKET}.s3.amazonaws.com`
 
 export const bundleUrl =
   stage === 'dev' ? 'http://localhost:8080/bundle.js' : `${staticBaseUrl}/js/bundle.js`
