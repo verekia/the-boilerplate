ff973301ae8fdb8af8b86e2667f96c51b4884537
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2019-08-22 22:12:55 +0900

    Use static folder for S3

--- src/_server/server-config.ts
+++ src/_server/server-config.ts
@@ -5,7 +5,7 @@ export const staticBaseUrl =
     ? 'http://localhost:8090'
     : stage === 'local-prod'
     ? 'http://localhost:8091'
-    : `https://${process.env.S3_BUCKET}.s3.amazonaws.com`
+    : `https://${process.env.S3_BUCKET}.s3.amazonaws.com/static`
 
 export const bundleUrl =
   stage === 'dev' ? 'http://localhost:8080/bundle.js' : `${staticBaseUrl}/js/bundle.js`
